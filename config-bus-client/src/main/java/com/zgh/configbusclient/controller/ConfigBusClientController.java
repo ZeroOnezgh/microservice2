07e3d5b8b065a00ea8299646ebc7114b8b12d106
zhanggh296 <zhanggh296@chinaunicom.cn>
2019-12-01 21:41:03 +0800

    properties再次更新3

--- config-bus-client/src/main/java/com/zgh/configbusclient/controller/ConfigBusClientController.java
+++ config-bus-client/src/main/java/com/zgh/configbusclient/controller/ConfigBusClientController.java
@@ -1,6 +1,7 @@
 package com.zgh.configbusclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by zhanggh296 on 2019/12/1.
  */
 @RestController
+@RefreshScope
 public class ConfigBusClientController {
     @Value("${neo.hello}")
     private String hello;
+
     @RequestMapping("/hello")
     public  String from(){
         return  this.hello;
